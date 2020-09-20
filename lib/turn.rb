32cb8c17a5be69f48bc52d59dd97edd1eba44474
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-09-20 03:04:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   index =
   input_to_index(user_input)
   if valid_move?(board, index)
-    move (board, index, player)
+    move(board, index, player)
     turn(board)
   end
   display_board(board)
