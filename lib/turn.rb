e2887cd5723b45002ad2a5e2a43f0247022f19a9
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-09-20 03:04:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,17 +36,12 @@ def position_taken?(board, index)
 
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.strip 
-  index = 
+  user_input = gets.strip
+  index =
   input_to_index(user_input)
   if valid_move?(board, index)
     move (board, index, player)
     turn(board)
   end
   display_board(board)
-end 
-
-
-
-
- 
+end
