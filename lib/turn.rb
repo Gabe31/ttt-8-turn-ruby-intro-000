423388b89fc73d3199e7500113a722e1a67f0d26
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-09-20 02:49:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,3 +33,19 @@ def position_taken?(board, index)
      return true
    end
  end
+
+ def turn_count(board)
+   counter = 0
+   board.each do |space|
+     if spaces == "X" || spaces == "O"
+       counter += 1
+     end
+   end 
+   counter
+ end 
+
+ def current_player(board)
+   turn_count(board) % 2 == 0 ? "X" : "O"
+ end
+ 
+ 
